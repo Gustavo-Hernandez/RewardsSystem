4a58c2764735f924e7cbe8ed2cef2a7ba74ce115
Gustavo-Hernandez <47768780+Gustavo-Hernandez@users.noreply.github.com>
2021-05-05 20:15:06 -0500

    Created Basic Navigators and Admin Hook

--- src/navigation/ClientNavigator.js
+++ src/navigation/ClientNavigator.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 
-const ContentNavigator = () => {
+const ClientNavigator = () => {
   return (
     <Switch>
       <Route exact path='/dashboard' component={Home} />
       <Route path='/' component={Home} />
-      {/* TODO: Replace for Page not found*/}
     </Switch>
   );
 };
-export default ContentNavigator;
+export default ClientNavigator;
