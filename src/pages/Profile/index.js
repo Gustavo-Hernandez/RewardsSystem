dd0a848ae6f0b04e3e708fa4ac4f25486695dec6
Luis Maawad <luismimaawad@hotmail.com>
2021-05-24 08:51:27 -0500

    Generated code for the admin update to user points

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -7,20 +7,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Context as AuthContext } from '../../context/AuthContext';
 import { Context as UserContext } from '../../context/UserDataContext';
 
+
 const Profile = ({ history }) => {
   const {
-    signout,
-    state: { email },
+      signout
   } = useContext(AuthContext);
 
   const {
-    query,
-    state: { points },
+      query,
+      state: { points, uid, email },
   } = useContext(UserContext);
 
-  useEffect(() => {
-    query();
-    // eslint-disable-next-line
+  useEffect(() =>{
+      query();
   }, []);
 
   return (
@@ -42,78 +41,52 @@ const Profile = ({ history }) => {
         <FontAwesomeIcon size='3x' icon={faChevronLeft} />
         <h3 style={{ marginLeft: '15px' }}>Volver</h3>
       </button>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Col sm={12} md={10} lg={8}>
-          <Row
-            className='mt-5'
+      <Row className='mt-5'>
+        <Col sm={4} md={2} style={{ display: 'flex' }}>
+          <div
             style={{
-              backgroundColor: 'white',
-              padding: '20px',
-              margin: '20px',
-              borderRadius: '20px',
+              border: '12px solid #1F53A1',
+              borderRadius: '50%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '280px',
+              height: '280px',
+              marginLeft: '25px',
             }}
           >
-            <Col
-              sm={12}
-              md={5}
-              lg={4}
-              style={{
-                display: 'flex',
-                marginLeft: '20px',
-                justifyContent: 'center',
-                overflow: 'visible',
-                maxWidth: '300px',
-              }}
+            <QRCode size={170} value={uid} />
+          </div>
+        </Col>
+        <Col
+          sm={6}
+          md={9}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            marginTop: '25px',
+            marginLeft: '25px',
+          }}
+        >
+          <Row>
+              <h4>Correo: { email }</h4>
+            <h4>Mis Puntos: { points }</h4>
+          </Row>
+          <Row>
+            <h4>Última Visita Registrada: {'2/12/2020'}</h4>
+          </Row>
+          <Row>
+            <Button
+              onClick={signout}
+              style={{ width: '150px', marginLeft: '12px', marginTop: '20px' }}
+              className='btn btn-danger'
             >
-              <div
-                style={{
-                  border: '12px solid #1F53A1',
-                  borderRadius: '50%',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  minWidth: '280px',
-                  height: '280px',
-                }}
-              >
-                <QRCode size={170} value={email} />
-              </div>
-            </Col>
-            <Col
-              sm={12}
-              md={6}
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                marginTop: '25px',
-                marginLeft: '25px',
-              }}
-            >
-              <Row>
-                <h4>Correo: {email}</h4>
-                <h4>Mis Puntos: {points}</h4>
-              </Row>
-              <Row>
-                <h4>Última Visita Registrada: {'2/12/2020'}</h4>
-              </Row>
-              <Row>
-                <Button
-                  onClick={signout}
-                  style={{
-                    width: '150px',
-                    marginLeft: '12px',
-                    marginTop: '20px',
-                  }}
-                  className='btn btn-danger'
-                >
-                  Cerrar Sesión
-                </Button>
-              </Row>
-            </Col>
+              Cerrar Sesión
+            </Button>
           </Row>
         </Col>
-      </div>
+      </Row>
     </div>
   );
 };
--- src/pages/ScanUser/index.js
+++ src/pages/ScanUser/index.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, {useContext, useState} from 'react';
 import NavigationBar from '../../components/NavigationBar';
-import { Button } from 'reactstrap';
 import QrReader from 'react-qr-reader';
+import {Context as UserContext} from "../../context/UserDataContext";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const ScanUser = () => {
   const [isScanning, setIsScanning] = useState(false);
-  const [scannedData, setScannedData] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [uid, setUid] = React.useState('');
+  const [pts, setPts] = React.useState(0);
+
+  const {
+    queryByID,
+    updatePoints,
+    state: { points, email },
+  } = useContext(UserContext);
 
   const toggleScan = () => {
     setIsScanning(!isScanning);
@@ -14,15 +23,33 @@ const ScanUser = () => {
   const handleScan = (data) => {
     if (data) {
       console.log(data);
-      setScannedData(data);
+      setUid(data);
+      queryByID(data);
+      setOpen(true);
       setIsScanning(false);
     }
   };
+
   const handleError = (err) => {
     if (err) {
       console.log('[QR_READER_ERROR]: ' + err);
     }
   };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    let change = parseInt(points) + parseInt(pts);
+    updatePoints({ change, uid });
+    setOpen(false);
+  };
+
+  const handleChange = event => {
+    let val = event.target.value;
+    setPts(val);
+  }
 
   return (
     <div>
@@ -41,7 +68,17 @@ const ScanUser = () => {
           {isScanning && (
             <QrReader delay={1000} onError={handleError} onScan={handleScan} />
           )}
-          {scannedData}
+          <Modal isOpen={open} toggle={handleClose} centered={true} >
+            <ModalHeader>Agregar/Quitar Puntos del Usuario</ModalHeader>
+            <ModalBody>
+              Usuario: { email } Puntos: { points }
+              <input type={"number"} placeholder={"Puntos"} onChange={handleChange}/>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="secondary" onClick={handleClose}>Cancelar</Button>{' '}
+              <Button color="primary" onClick={handleSubmit}>Agregar/Quitar</Button>
+            </ModalFooter>
+          </Modal>
         </div>
       </div>
     </div>
